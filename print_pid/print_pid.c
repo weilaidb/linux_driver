da42e5d10a2e35b1efe5f57aac5c70a2d9cf0221
Your Name <you@example.com>
2021-05-10 08:47:05 -0700

    add title leve,nr,vnr

--- print_pid/print_pid.c
+++ print_pid/print_pid.c
@@ -8,6 +8,26 @@
 #include <linux/list.h>
 
 extern struct task_struct init_task;
+//static int __init pid_vnr_init(pid_t nr)
+//{
+//	printk("into pid_vnr_init.\n");
+
+//	struct pid * kpid = find_get_pid(nr);
+
+//	printk("the level of the pid is:%d\n", kpid->level);
+//	printk("the pid of the pid is:%d\n", kpid->numbers[kpid->level].nr);
+
+//	int vnr = pid_vnr(kpid);
+//	printk("the pid_vnr result is:%d\n", vnr);
+
+//	//current pid, tgid, and out entry
+//	printk("the current pid  is:%d\n", nr);
+////	printk("the current tgid is:%d\n", current->tgid);
+//	printk("out pid_vnr_init\n");
+//	printk("module_refcount(THIS_MODULE):%d\n", module_refcount(THIS_MODULE) );
+//    return 0;
+//}
+
 //模块加载函数
 static int print_pid_init(void)
 {
@@ -16,13 +36,23 @@ static int print_pid_init(void)
 	struct list_head *pos;
 	int count = 0;
 	printk("Hello World enter begin:\n");
-	printk("%3s %10s\n", "PID", "NAME");
+	printk("%-6s %-30s %-10s %-10s %-10s\n", "PID", "NAME", "LEVEL", "NR", "VNR");
 	task = &init_task;
 	list_for_each(pos, &task->tasks)
 	{
 		p = list_entry(pos, struct task_struct, tasks);
 		count++;
-		printk("%d--->%s\n", p->pid, p->comm);
+
+		//other info
+		struct pid * kpid = find_get_pid(p->pid);
+		int vnr = pid_vnr(kpid);
+
+		printk("%-6d--->%-30s %-10d %-10d %-10d\n"
+					, p->pid, p->comm
+					, kpid->level
+					, kpid->numbers[kpid->level].nr
+					, vnr
+					);
 	}
 	printk("the number of process is:%d\n", count);
 
