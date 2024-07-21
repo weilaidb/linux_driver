b68992225266461717769a72acb0ac70d13bbbdd
duoduo <wxjlmr@126.com>
2024-07-21 03:34:22 -0700

    add dts of led

--- dtsofled/dtsofled.c
+++ dtsofled/dtsofled.c
@@ -8,10 +8,10 @@
 //必选
 //模块许可声明
 MODULE_LICENSE("GPL");
-static int __init dtsof_init(void);
-static void __exit dtsof_exit(void);
+static int __init dtsofled_init(void);
+static void __exit dtsofled_exit(void);
 
-static int __init dtsof_init(void)
+static int __init dtsofled_init(void)
 {
     int ret = 0;
 
@@ -24,23 +24,23 @@ static int __init dtsof_init(void)
     /* 1.找到backlight节点，路径是/backlight */
     bl_nd = of_find_node_by_path("/backlight");
     if(bl_nd == NULL) {
-        printk(KERN_ALERT "dtsof: can't find backlight node!\n");
+        printk(KERN_ALERT "dtsofled: can't find backlight node!\n");
         ret = -EINVAL;
         goto fail_findnd;
     }
-    printk(KERN_ALERT "dtsof: find backlight node!\n");
+    printk(KERN_ALERT "dtsofled: find backlight node!\n");
     /*2. 获取属性 */
     comppro = of_find_property(bl_nd, "compatible", NULL);
     if(comppro == NULL) {
-        printk(KERN_ALERT "dtsof: can't find compatible property!\n");
+        printk(KERN_ALERT "dtsofled: can't find compatible property!\n");
         ret = -EINVAL;
         goto fail_property;
     }
-    printk(KERN_ALERT "dtsof: find compatible property!\n");
-    printk(KERN_ALERT "dtsof: %s\n", (char *)comppro->value);
+    printk(KERN_ALERT "dtsofled: find compatible property!\n");
+    printk(KERN_ALERT "dtsofled: %s\n", (char *)comppro->value);
     ret = of_property_read_string(bl_nd, "status", &str);
     if(ret != 0) {
-        printk(KERN_ALERT "dtsof: can't find status property!\n");
+        printk(KERN_ALERT "dtsofled: can't find status property!\n");
         goto fail_rs;
     }
     else {
@@ -62,12 +62,12 @@ static int __init dtsof_init(void)
         goto fail_readele;
     }
     else {
-        printk(KERN_ALERT "dtsof: brightness-levels size: %d\n", elemsize);
+        printk(KERN_ALERT "dtsofled: brightness-levels size: %d\n", elemsize);
     }
     /* 申请内存*/
     brival = kmalloc(elemsize * sizeof(u32), GFP_KERNEL);
     if(brival == NULL) {
-        printk(KERN_ALERT "dtsof: kmalloc failed!\n");
+        printk(KERN_ALERT "dtsofled: kmalloc failed!\n");
         ret = -ENOMEM;
         goto fail_mem;
     }
@@ -80,7 +80,7 @@ static int __init dtsof_init(void)
     else {
         int i = 0;
         for(i = 0; i < elemsize; i++) {
-            printk(KERN_ALERT "dtsof: brightness-levels[%d]: %u\n", i, brival[i]);
+            printk(KERN_ALERT "dtsofled: brightness-levels[%d]: %u\n", i, brival[i]);
         }
     }
 
@@ -100,19 +100,19 @@ fail_read32:
     return ret;
 }
 //模块卸载函数
-static void __exit dtsof_exit(void)
+static void __exit dtsofled_exit(void)
 {
-    printk(KERN_ALERT "dtsof exit ok!\n");
+    printk(KERN_ALERT "dtsofled exit ok!\n");
 }
 
 
 
 
 //模块注册
-module_init(dtsof_init);
-module_exit(dtsof_exit);
+module_init(dtsofled_init);
+module_exit(dtsofled_exit);
 //可选
-MODULE_AUTHOR("dtsof");
-MODULE_DESCRIPTION("dtsof simple example!\n");
-MODULE_ALIAS("dtsof simplest example");
+MODULE_AUTHOR("dtsofled");
+MODULE_DESCRIPTION("dtsofled simple example!\n");
+MODULE_ALIAS("dtsofled simplest example");
 
